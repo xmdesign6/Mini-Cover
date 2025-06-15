e3c2d3b25c29fdc16e1e507baceaae0925473d4f
xmdesign6 <74769363+xmdesign6@users.noreply.github.com>
2025-06-15 13:55:09 +0800

    Update FooterComponent.vue

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -15,10 +15,10 @@
       <span>友善</span>
     </div>
     <div class="mt-2 space-x-2">
-      <span>© 2024</span>
-      <a href="https://bsgun.cn" 
+      <span>© 2025</span>
+      <a href="https://90svip.cn" 
          target="_blank"
-         class="font-bold text-green-600 hover:text-gray-600 transition-colors">梦爱吃鱼</a>
+         class="font-bold text-green-600 hover:text-gray-600 transition-colors">极客指北</a>
       <a v-if="icpNumber" 
          href="https://beian.miit.gov.cn/" 
          target="_blank"
